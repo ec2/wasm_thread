9e432077948d927d49373d1d039c23447d3648df
Eric Tu <tu.eric@hotmail.com>
2024-09-26 14:40:33 -0400

    fix module js script

--- src/wasm32/js/module_worker_start.js
+++ src/wasm32/js/module_worker_start.js
@@ -1,7 +1,7 @@
 let _worker;
 export function startWorker() {
   const worker = new Worker(
-    new URL('./web_worker_module.js', import.meta.url),
+    new URL('./web_worker_module.bundler.js', import.meta.url),
     {
       type: 'module',
       name: 'wasm_thread'
